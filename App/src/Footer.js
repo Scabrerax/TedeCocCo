888a98caee0dec4bb649533daea7c223c92c29f5
sebastiangi312 <sebastiangi312@gmail.com>
2020-09-25 21:12:01 -0500

    Revert "Merge pull request #8 from Scabrerax/Mello"
    This reverts commit 3e368bcfa7dbc97e432d51240767ba733b85aaeb, reversing
    changes made to cf4ab39aa245545498fa872183c406718c08e8a0.

--- App/src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-    return (
-        <footer className="text-center py-4 bg-dark text-white-50">
-            <small>Copyright &copy; Profe Ponganos 5</small>
-        </footer>
-    );
-}
--- App/src/Form.js
+++ App/src/Form.js
@@ -1,106 +1,92 @@
 import React, { Fragment } from "react";
 import { Col, Form as BootstrapForm, FormLabel } from "react-bootstrap";
-import { Card, ButtonToggle, CardText, CardTitle, CardSubtitle, CardBody } from 'reactstrap';
-import './Form.css';
 
 export const Form = ({ data }) => {
   const [title, ...subSections] = data;
 
   return (
-    <div className="col-sm-9 col-md-7 col-lg-5 mx-auto header">
-      <Card className="card-signin my-5">
-        <CardBody>
-          <Fragment>
-            <CardTitle className="text-center"><h1>{title.replace("_", " ")}</h1></CardTitle>
-            <BootstrapForm className="form-signin">
-              {subSections.map((subSection) => {
-                const [subTitle, ...elements] = subSection;
-                return (
-                  <Fragment key={subTitle}>
-                  <br/>
-                    <CardSubtitle><h2>{subTitle.replace("_", " ")}</h2></CardSubtitle>
-                    {elements.map((element) => {
-                      const label = element.split(" ")[0].replace("_", " ");
-                      const type = element.split(" ")[2];
+    <Fragment>
+      <h2>{title.replace("_", " ")}</h2>
+      <BootstrapForm>
+        {subSections.map((subSection) => {
+          const [subTitle, ...elements] = subSection;
 
-                      if (
-                        !(
-                          type === "radio" ||
-                          type === "checkbox" ||
-                          type === "select"
-                        )
-                      ) {
-                        return (
-                          <BootstrapForm.Group key={label}>
-                            <CardText><FormLabel>{label}:</FormLabel>
-                            </CardText>
-                            <BootstrapForm.Control as="input" type={type} />
-                          </BootstrapForm.Group>
-                        );
-                      }
+          return (
+            <Fragment key={subTitle}>
+              <h3>{subTitle.replace("_", " ")}</h3>
+              {elements.map((element) => {
+                const label = element.split(" ")[0].replace("_", " ");
+                const type = element.split(" ")[2];
 
-                      const options = element.split(" ").slice(4);
+                if (
+                  !(
+                    type === "radio" ||
+                    type === "checkbox" ||
+                    type === "select"
+                  )
+                ) {
+                  return (
+                    <BootstrapForm.Group key={label}>
+                      <FormLabel>{label}</FormLabel>
+                      <BootstrapForm.Control as="input" type={type} />
+                    </BootstrapForm.Group>
+                  );
+                }
 
-                      switch (type) {
-                        case "radio":
-                          return (
-                            <BootstrapForm.Group key={label}>
-                              <CardText><FormLabel>{label}:</FormLabel>
-                                <Col sm={10}>
-                                  {options.map((option) => (
-                                    <BootstrapForm.Check
-                                      key={option}
-                                      type="radio"
-                                      label={option}
-                                      name={label}
-                                    />
-                                  ))}
-                                </Col>
-                              </CardText>
-                            </BootstrapForm.Group>
-                          );
-                        case "checkbox":
-                          return (
-                            <BootstrapForm.Group key={label}>
-                              <CardText><FormLabel>{label}:</FormLabel>
-                                <Col sm={10}>
-                                  {options.map((option) => (
-                                    <BootstrapForm.Check
-                                      key={option}
-                                      type="checkbox"
-                                      label={option}
-                                      name={label}
-                                    />
-                                  ))}
-                                </Col>
-                              </CardText>
-                            </BootstrapForm.Group>
-                          );
-                        case "select":
-                          return (
-                            <BootstrapForm.Group key={label}>
-                              <CardText><FormLabel>{label}:</FormLabel>
-                                <BootstrapForm.Control as="select">
-                                  {options.map((option) => (
-                                    <option key={option}>{option}</option>
-                                  ))}
-                                </BootstrapForm.Control>
-                              </CardText>
-                            </BootstrapForm.Group>
-                          );
-                        default:
-                          return <Fragment key={label} />;
-                      }
-                    })}
-                  </Fragment>
-                );
+                const options = element.split(" ").slice(4);
+
+                switch (type) {
+                  case "radio":
+                    return (
+                      <BootstrapForm.Group key={label}>
+                        <FormLabel>{label}</FormLabel>
+                        <Col sm={10}>
+                          {options.map((option) => (
+                            <BootstrapForm.Check
+                              key={option}
+                              type="radio"
+                              label={option}
+                              name={label}
+                            />
+                          ))}
+                        </Col>
+                      </BootstrapForm.Group>
+                    );
+                  case "checkbox":
+                    return (
+                      <BootstrapForm.Group key={label}>
+                        <FormLabel>{label}</FormLabel>
+                        <Col sm={10}>
+                          {options.map((option) => (
+                            <BootstrapForm.Check
+                              key={option}
+                              type="checkbox"
+                              label={option}
+                              name={label}
+                            />
+                          ))}
+                        </Col>
+                      </BootstrapForm.Group>
+                    );
+                  case "select":
+                    return (
+                      <BootstrapForm.Group key={label}>
+                        <FormLabel>{label}</FormLabel>
+                        <BootstrapForm.Control as="select">
+                          {options.map((option) => (
+                            <option key={option}>{option}</option>
+                          ))}
+                        </BootstrapForm.Control>
+                      </BootstrapForm.Group>
+                    );
+                  default:
+                    return <Fragment key={label} />;
+                }
               })}
-
-              <ButtonToggle className="offset-9" size="md" color="primary">Save</ButtonToggle>{' '}
-            </BootstrapForm>
-          </Fragment>
-        </CardBody>
-      </Card>
-    </div>
+            </Fragment>
+          );
+        })}
+      </BootstrapForm>
+    </Fragment>
   );
 };
