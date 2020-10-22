71c13890858d747b9f49a6e45a32e84f6b53fc40
juanpa800 <canojuanpa@gmail.com>
2020-10-21 23:51:11 -0500

    feat: add more rules

--- App/src/utils.js
+++ App/src/utils.js
@@ -2,6 +2,12 @@ const rules = {
   "*": { required: "*Campo Requerido" },
   ">": (minLength) => ({ minLength:{value: Number.parseInt(minLength), message: `*Minima longitud ${minLength} caracteres`} }),
   "<": (maxLength) => ({ maxLength:{value: Number.parseInt(maxLength), message: `*Maxima longitud ${maxLength} caracteres`} }),
+  "m": (max) => ({ max:{value: Number.parseInt(max), message: `*El valor minimo es ${max}`} }),
+  "p": (min) => ({ min:{value: Number.parseInt(min), message: `*El valor maximo es ${min}`} }),
+  "u": {pattern: {value:/\b[A-Z]+\b/ , message: `*Solo se permiten mayusculas`}},
+  "l": {pattern: {value:/\b[a-z]+\b/ , message: `*Solo se permiten minusculas`}},
+  "s": {pattern: {value:/^\S+$/, message: `*No se permiten espacios`}},
+  "t": {pattern: {value:/\b[a-zA-Z]+\b/ , message: `*Solo se permite texto`}},
 };
 
 export const parseRules = (rulesArray) =>
